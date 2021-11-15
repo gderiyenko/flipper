25c297ebfd5a106be573d63e1b4cf39abacf073e
Gleb Deriyenko <gderiyenko@Glebs-MacBook-Pro.local>
2021-11-16 00:55:54 +0200

    Add words to Database

--- components/Library/LibraryWords.tsx
+++ components/Library/LibraryWords.tsx
@@ -18,50 +18,11 @@ function openDatabase() {
 
 const db = openDatabase();
 
-function Items(group_id: number) {
-  const [items, setItems] = React.useState(null);
-  let colorScheme = useColorScheme();
-
-  React.useEffect(() => {
-    db.transaction((tx) => {
-      tx.executeSql(
-        `select * from words where group_id = ?;`,
-        [group_id],
-        (_, { rows: { _array } }) => setItems(_array)
-      );
-    });
-  }, []);
-
-  if (items === null || items.length === 0) {
-    return null;
-  }
-
-  return (
-    <View style={styles.sectionContainer}>
-      <Text style={styles.sectionHeading}>Groups:</Text>
-      {items.map(({ id, value }) => (
-        <TouchableOpacity
-          key={id}
-          onPress={() => onPressItem && onPressItem(id, value)}
-          style={{
-            marginTop: 10,
-            borderColor: "#000",
-            borderWidth: 1,
-            padding: 8,
-          }}
-        >
-          <Text style={colorScheme == 'dark' ? { color: "white", } : { color: "black", }}>{value}</Text>
-        </TouchableOpacity>
-      ))}
-    </View>
-  );
-}
-
-
 export default function LibraryWords(all: any) {
   const [forceUpdate, forceUpdateId] = useForceUpdate();
   const [groupId, setGroupId] = useState(all.route.params.group_id);
   const [groupName, setGroupName] = useState(all.route.params.group_name);
+  const [items, setItems] = useState([]);
 
 
   const [nativeText, setNative] = React.useState(null);
@@ -72,36 +33,37 @@ export default function LibraryWords(all: any) {
   React.useEffect(() => {
     db.transaction((tx) => {
       tx.executeSql(
-        "create table if not exists words (id integer primary key not null, group_id integer, value text);"
+        "create table if not exists words (id integer primary key not null, group_id integer, native_text text, translate_text text );"
       );
     });
+    db.transaction((tx) => {
+      tx.executeSql(
+        "select * from words where group_id = ?;",
+        [groupId],
+        (_, { rows: { _array } }) => setItems(_array.reverse())
+      );
+    },
+      (err) => { console.log(err) },
+      () => { console.log(items) },
+    );
   }, []);
 
-  const add = (group_id: number, text: string) => {
-    // is text empty?
-    if (text === null || text === "") {
-      return false;
-    }
+  const add = (group_id: number, native_text: string, translate_text: string) => {
 
+    // TODO: is text empty?
+    if (native_text === null || translate_text === null) { return false; }
+    console.log(group_id, native_text, translate_text);
     db.transaction(
       (tx) => {
-        tx.executeSql("insert into words (group_id, value) values (?, ?)", [group_id, text]);
-        tx.executeSql("select * from words", [], (_, { rows }) =>
-          console.log(JSON.stringify(rows))
+        tx.executeSql("insert into words (group_id, native_text, translate_text) values (?, ?, ?)", [group_id, native_text, translate_text]);
+        tx.executeSql(
+          "select * from words where group_id = ?;",
+          [groupId],
+          (_, { rows: { _array } }) => setItems(_array.reverse())
         );
       },
-      null,
-      forceUpdate
-    );
-  };
-
-  const remove = (id: number) => {
-    db.transaction(
-      (tx) => {
-        tx.executeSql(`delete from words where id = ?;`, [id]);
-      },
-      null,
-      forceUpdate
+      (err) => { console.log(err) },
+      () => { forceUpdate; }
     );
   };
 
@@ -132,102 +94,83 @@ export default function LibraryWords(all: any) {
       </View>
       {/* END: Header */}
 
-
-
       <ScrollView style={[styles.listArea, colorScheme == 'dark' ? { backgroundColor: "#292c34", } : { backgroundColor: "#f0f0f0", }]}>
-        <Text style={{ color: 'red' }}>Add words</Text>
-        <View style={styles.addWords}>
-          {/* Native */}
-          <TextInput
-            style={[styles.multiInput, ]}
-            onChangeText={(text) => setNative(text)}
-            onSubmitEditing={() => {
-              add(groupId, nativeText ?? '');
-              setTranslate(null);
-            }}
-            placeholder="Native"
-            placeholderTextColor={colorScheme == 'dark' ? "white" : "black"}
-            value={nativeText}
-            multiline={true}
-          />
-          {/* END: Native */}
 
-          <View style={{
-            width: '100%',
-            flexDirection: 'row',
-            justifyContent: 'flex-end',
-            margin: 5,
-            padding: 5,
-          }}>
-            <View style={{
-              backgroundColor: equal ? "red" : "#F08080",
-              padding: 5,
-              borderRadius: 4,
-            }}>
-              <Text style={{ color: 'white' }}>
-                {(nativeText ?? '').split(/\r\n|\r|\n/).length - 1} lines
-              </Text>
+        {/* Add Words Form */}
+        <>
+          <Text style={[styles.sectionHeading, { color: "red", }]}>Add words:</Text>
+          <View style={styles.addWords}>
+            {/* Native */}
+            <TextInput
+              style={[styles.multiInput, { color: colorScheme == 'dark' ? "white" : "black" }]}
+              onChangeText={(text) => setNative(text)}
+              placeholder="Native"
+              placeholderTextColor={colorScheme == 'dark' ? "white" : "black"}
+              value={nativeText}
+              multiline={true}
+            />
+
+            {/* Separator + Lines Counter */}
+            <View style={styles.addWordsLines}>
+              <View style={[styles.addWordsLinesCounter, { backgroundColor: equal ? "red" : "#F08080", }]}>
+                <Text style={{ color: 'white' }}>
+                  {(nativeText ?? '').split(/\r\n|\r|\n/).length - 1} lines
+                </Text>
+              </View>
             </View>
+            <View style={{ width: '100%', backgroundColor: 'red', borderRadius: 1, height: 1, }}></View>
+            <View style={styles.addWordsLines}>
+              <View style={[styles.addWordsLinesCounter, { backgroundColor: equal ? "red" : "#F08080", }]}>
+                <Text style={{ color: 'white' }}>
+                  {(translateText ?? '').split(/\r\n|\r|\n/).length - 1} lines
+                </Text>
+              </View>
+            </View>
+
+            {/* Translate */}
+            <TextInput
+              style={[styles.multiInput, { color: colorScheme == 'dark' ? "white" : "black" }]}
+              onChangeText={(text) => setTranslate(text)}
+              placeholder="Translate"
+              placeholderTextColor={colorScheme == 'dark' ? "white" : "black"}
+              value={translateText}
+              multiline={true}
+            />
           </View>
 
-          <View style={{ width: '100%', backgroundColor: 'red', borderRadius: 1, height: 1, }}></View>
-
-          <View style={{
-            width: '100%',
-            flexDirection: 'row',
-            justifyContent: 'flex-end',
-            margin: 5,
-            padding: 5,
-          }}>
-            <View style={{
-              backgroundColor: equal ? "red" : "#F08080",
-              padding: 5,
-              borderRadius: 4,
-            }}>
-              <Text style={{ color: 'white' }}>
-                {(translateText ?? '').split(/\r\n|\r|\n/).length - 1} lines
-              </Text>
-            </View>
+          {/* Submit */}
+          <View style={{ alignItems: 'flex-end', marginRight: 15 }}>
+            <Button
+              onPress={() => { add(groupId, nativeText ?? "", translateText ?? ""); }}
+              title="Add to Library"
+              buttonStyle={{ backgroundColor: "red", }}
+              disabled={!equal}
+              disabledStyle={{ backgroundColor: "#F08080", }}
+              disabledTitleStyle={{ color: 'white' }}
+              type='solid'
+              accessibilityLabel="Learn more about this purple button"
+            />
           </View>
+        </>
+        {/* End: Add Words Form */}
 
-          {/* Translate */}
-          <TextInput
-            style={styles.multiInput}
-            onChangeText={(text) => setTranslate(text)}
-            onSubmitEditing={() => {
-              add(groupId, translateText ?? '');
-              setTranslate(null);
-            }}
-            placeholder="Translate"
-            placeholderTextColor={colorScheme == 'dark' ? "white" : "black"}
-            value={translateText}
-            multiline={true}
-          />
-          {/* END: Translate */}
-        </View>
-
-        <View style={{ alignItems: 'flex-end', marginRight: 15 }}>
-          <Button
-            onPress={() => { console.log(1) }}
-            title="Add to Library"
-            buttonStyle={{ backgroundColor: "red", }}
-            disabled={!equal}
-            disabledStyle={{ backgroundColor: "#F08080", }}
-            disabledTitleStyle={{ color: 'white' }}
-
-            type='solid'
-            accessibilityLabel="Learn more about this purple button"
-          />
-        </View>
-
-        <Items
-          key={`forceupdate-todo-${forceUpdateId}`}
-          group_id={groupId}
-        />
+        {/* List Existing Words */}
+        {items.length > 0 ? <Text style={[styles.sectionHeading, { color: colorScheme == 'dark' ? "white" : "black", }]}>List:</Text> : <></>}
+        {items.map(({ id, native_text, translate_text }) => (
+          <TouchableOpacity
+            key={id}
+            onPress={() => { }}
+            style={[styles.wordItem, { borderColor: colorScheme == 'dark' ? "white" : "black", }]}
+          >
+            <Text style={{ color: colorScheme == 'dark' ? "white" : "black", }}>{native_text}</Text>
+            <View style={{ width: '100%', backgroundColor: 'red', borderRadius: 1, height: 1, }}></View>
+            <Text style={{ color: colorScheme == 'dark' ? "white" : "black", }}>{translate_text}</Text>
+          </TouchableOpacity>
+        ))}
       </ScrollView>
       {/* END: List */}
 
-    </View>
+    </View >
   );
 }
 
@@ -258,6 +201,7 @@ const styles = StyleSheet.create({
     margin: 16,
     padding: 8,
   },
+
   addWords: {
     paddingTop: 10,
     marginBottom: 10,
@@ -268,16 +212,35 @@ const styles = StyleSheet.create({
     margin: 16,
     padding: 8,
   },
+  addWordsLines: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    margin: 5,
+    padding: 5,
+  },
+  addWordsLinesCounter: {
+    padding: 5,
+    borderRadius: 4,
+  },
+
   multiInput: {
     paddingVertical: 10,
   },
-  sectionContainer: {
-    marginBottom: 16,
-    marginHorizontal: 16,
-  },
+
+  // Word List
   sectionHeading: {
+    marginHorizontal: 16,
     fontSize: 18,
-    color: "red",
     marginBottom: 8,
+  },
+  wordItem: {
+    paddingTop: 10,
+    marginBottom: 10,
+    borderRadius: 4,
+    borderWidth: 1,
+    flex: 1,
+    margin: 16,
+    padding: 8,
   },
 });
