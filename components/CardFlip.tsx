ec2894e709822ea0b1ee38d2b2a103f775d32805
Gleb Deriyenko <gderiyenko@Glebs-MacBook-Pro.local>
2021-11-16 04:41:48 +0200

    Card Flip Implemented

--- components/CardFlip.tsx
+++ components/CardFlip.tsx
@@ -3,26 +3,20 @@ import React, { useState } from 'react';
 import { StyleSheet, TouchableOpacity } from 'react-native';
 import { Text, View } from './Themed';
 
-export default function CardFlip() {
+export default function CardFlip(props) {
 
   let [side, setSide] = useState(0); // Card's side [0, 1]
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.card} onPressIn={() => setSide((++side) % 2)}>
+      <TouchableOpacity style={styles.card} onPress={() => setSide((++side) % 2)}>
         <View style={styles.card} lightColor="#f7f7f7" darkColor="#292c34">
-
-          {side == 0 ? (
-            // FRONT SIDE
-            <Text lightColor="red" darkColor="white" style={{ fontSize: 35 }}>alalla</Text>
-          ) : (
-            // BACK SIDE
-            <View lightColor="#f7f7f7" darkColor="#292c34">
-              <Ionicons size={35} name="language-outline" color="red" />
-              <Text lightColor="red" darkColor="white" style={{ fontSize: 35 }}>translate</Text>
-            </View>
-          )}
-
+          {side == 0 ? <Ionicons size={35} name="language-outline" color="red" /> : <></>}
+          <View lightColor="#f7f7f7" darkColor="#292c34">
+            <Text lightColor="black" darkColor="white" style={{ fontSize: 35 }}>
+              {side == 0 ? props.translate : props.native}
+            </Text>
+          </View>
         </View>
       </TouchableOpacity>
     </View>
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,14 +1,26 @@
 import * as React from 'react';
 import { StyleSheet, Pressable } from 'react-native';
-
+import * as SQLite from "expo-sqlite";
 import { Text, View } from '../components/Themed';
 import { Ionicons } from '@expo/vector-icons';
+import { Swipeable } from 'react-native-gesture-handler';
 import { RootTabScreenProps } from '../types';
 import CardFlip from '../components/CardFlip';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>, groupParams?:any) {
+function openDatabase() {
+  const db = SQLite.openDatabase("db.db");
+  return db;
+}
+
+const db = openDatabase();
+
+export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>, groupParams?: any) {
   let [currentGroupName, setCurrentGroupName] = React.useState(groupParams.name ?? "All Groups");
   let [currentGroupWhere, setCurrentGroupWhere] = React.useState(groupParams.where ?? "1");
+
+  const [reload, cardReload] = React.useState(false);
+  const [cardRef, setRef] = React.useState();
+
   return (
     <View style={{ flex: 1, }}>
 
@@ -16,12 +28,12 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
       <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 15 }}>
         {/* Left */}
         <View style={{ width: '33%' }}>
-        <Text style={{ }}> </Text>
+          <Text style={{}}> </Text>
         </View>
 
         {/* Center */}
         <View style={{ width: '33%', flexDirection: 'row', justifyContent: 'center' }}>
-          <Text style={{ }}>{currentGroupName}</Text>
+          <Text style={{}}>{currentGroupName}</Text>
         </View>
 
         {/* Right */}
@@ -30,7 +42,7 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
           style={({ pressed }) => ({
             opacity: pressed ? 0.5 : 1,
             width: '33%',
-            flexDirection: 'row', 
+            flexDirection: 'row',
             justifyContent: 'flex-end'
           })}>
           <Ionicons
@@ -42,8 +54,27 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
         </Pressable>
       </View>
       {/* END: Header */}
+      <Swipeable
+        containerStyle={{ width: '100%', height: '100%' }}
+        ref={(ref: any) => setRef(ref)}
+        renderRightActions={() => {
+          return (
+            <View style={[{ height: '100%', width: "100%" },]}>
+            </View>
+          )
+        }}
+        onSwipeableRightWillOpen={
+          () => {
+            cardRef.close()
+            cardReload(true)
+            setTimeout(() => cardReload(false), 200);
+          }
+        }>
+        <View style={{ width: '100%', height: '100%', }}>
+          {reload? <></> : <CardFlip native={"a"} translate={"b"}></CardFlip>}
+        </View>
 
-      <CardFlip/>
+      </Swipeable>
 
     </View>
   );
